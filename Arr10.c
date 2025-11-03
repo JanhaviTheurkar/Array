bfdbf9ac939638e8ea329130ea51597fdc8da163
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-11-03 21:03:44 +0530

    Add array input and output functionality in Arr10.c

---
+++ Arr10.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int n, a[100];
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    for(int i=0;i<n;i++) printf("%d ",a[i]);
+}
