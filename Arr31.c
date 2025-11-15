6955b897cee9308c08d77d22aff09ca459232493
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-11-15 20:22:39 +0530

    Add program to calculate diagonal sum of a matrix

---
+++ Arr31.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int sum = 0;
+    for(int i=0;i<3;i++)
+        sum += a[i][i];
+    printf("Diagonal sum = %d", sum);
+}
